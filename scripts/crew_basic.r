a02d438ca0f2ea55cdc6d4d9a0e1019392b70b70
Ven Popov <vencislav.popov@gmail.com>
2024-12-17 05:36:38 +0100

    replace mixtur with my fork venpopov/mixtur@rescale_parameters

--- scripts/crew_basic.r
+++ scripts/crew_basic.r
@@ -1,12 +1,13 @@
 library(crew)
+library(dplyr)
 
-ctrl <- crew_controller_local(workers = 12)
+ctrl <- crew_controller_local(workers = 12, seconds_idle = 30)
 ctrl$start()
 
 # grid of parameters to iterate over
 pars <- expand.grid(
   n = c(20, 50, 100),
-  kappa = seq(1, 16, 2),
+  kappa = seq(1, 16, 0.2),
   p_mem = seq(0.6, 1, 0.1)
 )
 
@@ -25,3 +26,18 @@ recover_mixture2p <- function(n, kappa, p_mem) {
       mutate(n = n, kappa = kappa, p_mem = p_mem)
   )
 }
+
+
+res <- ctrl$map(
+  command = recover_mixture2p(n, kappa, p_mem),
+  iterate = pars,
+  packages = c('dplyr','bmm'),
+  globals = list(recover_mixture2p = recover_mixture2p)
+)
+
+ctrl$terminate()
+
+microbenchmark::microbenchmark(
+  purrr::pmap_dfr(pars, recover_mixture2p),
+  times = 10
+)
