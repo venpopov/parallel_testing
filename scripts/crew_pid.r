157c7a40cbd31381ef0cc7bc206ef83e5674fed9
Ven Popov <vencislav.popov@gmail.com>
2024-12-17 03:42:26 +0100

    add mini crew test with pids

---
+++ scripts/crew_pid.r
@@ -0,0 +1,35 @@
+library(crew)
+
+# initialize the controller
+ctrl <- crew_controller_local(workers = 4, seconds_idle = 5)
+
+# start the controller. THis launches a single R process
+ctrl$start()
+
+# Use `push()` to submit a task to the controller
+ctrl$push(name = "task 1", command = {
+  Sys.sleep(5)
+  ps::ps_pid()
+  }) 
+ctrl$push(name = "task 2", command = {
+  Sys.sleep(5)
+  ps::ps_pid()
+  }) 
+ctrl$push(name = "task 3", command = {
+  Sys.sleep(5)
+  ps::ps_pid()
+  }) 
+ctrl$push(name = "task 4", command = {
+  Sys.sleep(5)
+  ps::ps_pid()
+  }) 
+ctrl$push(name = "task 5", command = {
+  Sys.sleep(5)
+  ps::ps_pid()
+  }) 
+
+ctrl$wait(mode = "all")
+
+res <- ctrl$collect()
+res
+res$result
